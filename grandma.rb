0770e87922b1ee83f981144f08eb36624a10d40e
Reed S. MacConnell <learn22ruby@gmail.com>
2013-07-31 13:42:07 -0400

    needed the answer in the back of the book for help on this one. Did the right things, but put them in the wrong order

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,17 @@
 puts 'Hello, youngster. What do you want?'
-info = gets.chomp
 
+while true
+	info = gets.chomp
+	if info == 'BYE'
+		puts 'BYE SWEETIE!'
+		break
+		
+	end
 
+	if info != info.upcase
+		puts 'HUH! SPEAK UP, SONNY!'
+	else
+		random_year = 1930 + rand(21)
+		puts 'NO, NOT SINCE ' + random_year.to_s + '!'
+	end
+end

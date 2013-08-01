f895776d7d71e2e05909c3e752b2864840bc9906
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-01 10:03:24 -0400

    extended grandma & added my own extra

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,24 @@
 puts 'Hello, youngster. What do you want?'
+bye_count = 0
 
 while true
 	info = gets.chomp
-	if info == 'BYE'
-		puts 'BYE SWEETIE!'
-		break
-		
-	end
 
-	if info != info.upcase
-		puts 'HUH! SPEAK UP, SONNY!'
+	if info == 'BYE'
+		bye_count = bye_count + 1
+		puts 'WHAT\'S THAT NOW? PIE!'
 	else
+		bye_count = 0
+	end
+	break if bye_count >= 3
+	response = if info != info.upcase
+		'HUH! SPEAK UP, SONNY!'
+	elsif info == info.upcase && info != 'BYE'
 		random_year = 1930 + rand(21)
 		puts 'NO, NOT SINCE ' + random_year.to_s + '!'
 	end
+	puts response
 end
+
+puts 'BYE-BYE, CUPCAKE!'
+
